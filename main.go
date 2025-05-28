fafb155f494953ec9be6f38eedd3820a106c61f9
Luiz Cornelli <luizcornelli.dev@gmail.com>
2025-05-28 10:24:50 -0300

    Update main.go

--- main.go
+++ main.go
@@ -1,15 +1,26 @@
 package main
 
 import (
-	"fmt"
-	"time"
+    "fmt"
+    "net/http"
+    "time"
 )
 
 func main() {
-	fmt.Println("Full Cycle Rocks!!")
-	
-	// Mantém o container rodando
-	for {
-		time.Sleep(1 * time.Hour) // Mantém o processo ativo
-	}
+    fmt.Println("Full Cycle Rocks!!")
+
+    // Configurar o servidor HTTP em uma goroutine
+    go func() {
+        http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+            w.Header().Set("Content-Type", "text/html")
+            fmt.Fprintf(w, "<h1>Full Cycle Rocks!!</h1>")
+        })
+        
+        http.ListenAndServe(":8081", nil)
+    }()
+
+    // Mantém o container rodando
+    for {
+        time.Sleep(1 * time.Hour)
+    }
 }
